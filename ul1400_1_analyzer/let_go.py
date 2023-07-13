c1a040145059c190c2f0a1c9831674155848ad7e
Jonathan Casey <jonathan.casey@voltserver.com>
2023-07-13 14:13:32 -0400

    Let-go assessment implemented, with waveform utilities added to support data manipulation in analysis.  This DOES fix a clear typo in UL1400-1 regarding the let-go threshold equation for current.

--- ul1400_1_analyzer/let_go.py
+++ ul1400_1_analyzer/let_go.py
@@ -5,3 +5,252 @@ per UL1400-1.
 Module Attributes:
   N/A
 """
+import math
+
+from ul1400_1_analyzer.utils import waveform as waveform_utils
+
+
+
+def find_time_regions_below_letgo(current_waveform:dict[float, float]|None=None,
+        voltage_waveform:dict[float, float]|None=None,
+        env_conditions:str|None=None, start_time:float|None=None,
+        min_window_duration:float=3) -> tuple[tuple[float, float], ...]:
+    """
+    Finds the time regions that are below the let-go threshold.  When the
+    appropriate minimum recovery duration is provided, this can be used to
+    identify regions that may qualify as Fault Recovery Periods per UL1400-1.
+
+    Args:
+      current_waveform [s, A]: The fault current waveform to evaluate if it is
+        below let-go.  Each entry is a point on the waveform, with the key being
+        the time and the value being the electrical current value.  This can be
+        omitted if only the fault voltage waveform will be used, but this does
+        potentially produce excessively conservative results.  The time values
+        do not need to align with the voltage waveform time values to be valid.
+        Time value keys in the dictionary do not need to be ordered.
+      voltage_waveform [s, V]: The fault voltage waveform to evaluate if it is
+        below let-go.  Each entry is a point on the waveform, with the key being
+        the time and the value being the electrical voltage value.  This can be
+        omitted if only the fault current waveform will be used, but this does
+        potentially produce excessively conservative results.  The time values
+        do not need to align with the current waveform time values to be valid.
+        Time value keys in the dictionary do not need to be ordered.  If this is
+        used, the environmental conditions must be provided.
+      env_conditions: The environmental conditions to be used for evaluating
+        let-go.  This is only required if the fault voltage waveform is
+        provided, as this is the only waveform to which it applies.  The valid
+        values are "wet" and "dry".
+      start_time [s]: The time above which to analyze waveform data.  This is
+        useful if the fault is introduced at some mid-point in the time series
+        and may contain invalid data earlier than that.  This can be omitted to
+        use all data in the waveforms.
+      min_window_duration [s]: The minimum time duration to use as a window
+        size for evaluating data.  For compliance, this should likely be the
+        minimum Fault Recovery Period duration required by UL1400-1.
+
+    Returns:
+      time_regions_below_letgo [s]: These are the time regions identified as
+        being below the let-go threshold per the data and parameters specified.
+        It REQUIRES the correct parameters to be specified (e.g.
+        min_window_duration) in order for this to suggest the analysis is
+        compliant with UL1400-1.  Each entry in the list is a pair of start and
+        end time values that each identify the continuous region where the
+        waveforms are below let-go for all time windows of min_window_duration
+        or larger.  An empty list indicates no regions were identified to be
+        below the let-go threshold per the data and parameters specified.
+    """
+    both_waveforms = {
+        'current': current_waveform,
+        'voltage': voltage_waveform,
+    }
+    time_regions_below_letgo:list[tuple[float, float]] = []
+
+    for measurement_type, waveform in both_waveforms.items():
+        if waveform is None:
+            continue
+
+        working_region = None
+        min_start_time = start_time if start_time is not None else min(
+                waveform.keys())
+        target_end_time = max(waveform.keys())
+        target_start_time:float|None = target_end_time - min_window_duration
+
+        while target_start_time is not None \
+                and target_start_time >= min_start_time:
+            segment = waveform_utils.get_segment_from_waveform(waveform,
+                    target_start_time, target_end_time, min_window_duration,
+                    'start')
+
+            if is_below_letgo(measurement_type, segment.values(),
+                    env_conditions):
+                new_region = (min(segment.keys(), max(segment.keys())))
+                working_region = waveform_utils.expand_region(working_region,
+                        new_region)
+            elif working_region is not None:
+                time_regions_below_letgo.append(working_region)
+                working_region = None
+
+            remaining_end_times = [t for t in waveform.keys()
+                    if t < target_end_time]
+            if remaining_end_times:
+                target_end_time = max(remaining_end_times)
+                target_start_time = target_end_time - min_window_duration
+            else:
+                target_start_time = None
+
+        if working_region is not None:
+            time_regions_below_letgo.append(working_region)
+
+    return waveform_utils.merge_regions(time_regions_below_letgo)
+
+
+
+def is_below_letgo(measurement_type:str, segment_values:list[float]|None,
+        env_conditions:str|None=None) -> bool|None:
+    """
+    Determines if the provided segment of data is below the let-go threshold.
+    When the appropriate data is provided, this can be used to assess if the
+    segment may qualify as a Fault Recovery Period per UL1400-1.
+
+    Args:
+      measurement_type: The type of measurement represented in the segment data
+        provided.  Valid values are "current" and "voltage".
+      segment_values: The values of the segment of the waveform to evaluate (
+        i.e. times associated with each value are not needed and should not be
+        included).
+      env_conditions: The environmental conditions to be used for evaluating
+        let-go.  This is only required if the measurement type provided is
+        "voltage", as this is the only type to which it applies.  The valid
+        values are "wet" and "dry".
+
+    Returns:
+      _: True if the segment is below the appropriate let-go threshold based on
+        the provided arguments; False if segment data exists and provably
+        exceeded the let-go threshold; None if no segment data provided and
+        therefore could not asses.
+
+    Raise:
+      ValueError: Raised if an invalid value for the measurement type is
+        provided.
+    """
+    if measurement_type == 'current':
+        return is_current_below_letgo(segment_values)
+    if measurement_type == 'voltage':
+        return is_voltage_below_letgo(segment_values, env_conditions)
+    raise ValueError('Measurement type must be "voltage" or "current", got'
+            f' "{measurement_type}"')
+
+
+
+def is_current_below_letgo(segment_values:list[float]|None) -> bool|None:
+    """
+    Determines if the provided segment of data is below the let-go threshold for
+    current.  When the appropriate data is provided, this can be used to assess
+    if the segment may qualify as a Fault Recovery Period per UL1400-1.
+
+    Args:
+      segment_values: The electrical current values of the segment of the
+        waveform to evaluate (i.e. times associated with each value are not
+        needed and should not be included).
+
+    Returns:
+      _: True if the segment is below the appropriate let-go threshold based on
+        the provided arguments; False if segment data exists and provably
+        exceeded the let-go threshold; None if no segment data provided and
+        therefore could not asses.
+
+    Raise:
+      ValueError: Raised if an ambiguous condition with respect to UL1400-1 is
+        encountered and could not be resolved.
+    """
+    if segment_values is None:
+        return None
+
+    a_to_ma_scalar = 1000
+
+    peak_ma = max(segment_values) * a_to_ma_scalar
+    mean_ma = sum(segment_values) / len(segment_values) * a_to_ma_scalar
+
+    if mean_ma < 0:
+        # This should likely invert or consider worst peak, but technically is
+        #   undefined
+        raise ValueError('DC values less than 0 are not supported for let-go:'
+                f' {mean_ma} mA')
+    if mean_ma <= 4.21:
+        limit_ma = 5 * math.sqrt(2)
+    elif mean_ma <= 30:
+        # NOTE: Technically this is 33.3, not 3.33; but it is easy to determine
+        #   that 33.3 is a typo
+        limit_ma = 3.33 + 0.89 * mean_ma
+    else:
+        # This should likely be a failure, but technically is undefined
+        raise ValueError('DC values greater than 30 mA are not supported for'
+                f' let-go: {mean_ma} mA')
+
+    return peak_ma <= limit_ma
+
+
+
+def is_voltage_below_letgo(segment_values:list[float]|None,
+        env_conditions:str) -> bool|None:
+    """
+    Determines if the provided segment of data is below the let-go threshold for
+    voltage.  When the appropriate data is provided, this can be used to assess
+    if the segment may qualify as a Fault Recovery Period per UL1400-1.
+
+    Args:
+      segment_values: The electrical voltage values of the segment of the
+        waveform to evaluate (i.e. times associated with each value are not
+        needed and should not be included).
+      env_conditions: The environmental conditions to be used for evaluating
+        let-go.  This is only required if the measurement type provided is
+        "voltage", as this is the only type to which it applies.  The valid
+        values are "wet" and "dry".
+
+    Returns:
+      _: True if the segment is below the appropriate let-go threshold based on
+        the provided arguments; False if segment data exists and provably
+        exceeded the let-go threshold; None if no segment data provided and
+        therefore could not asses.
+
+    Raise:
+      ValueError: Raised if an ambiguous condition with respect to UL1400-1 is
+        encountered and could not be resolved; or if an invalid value for the
+        environmental condition is provided.
+    """
+    if segment_values is None:
+        return None
+
+    if env_conditions not in ['wet', 'dry']:
+        raise ValueError('Environmental conditions must be specified as wet or'
+                f' dry; got {env_conditions}')
+
+    peak = max(segment_values)
+    mean = sum(segment_values) / len(segment_values)
+
+    if mean < 0:
+        # This should likely invert or consider worst peak, but technically is
+        #   undefined
+        raise ValueError('DC values less than 0 are not supported for let-go:'
+                f' {mean} V')
+
+    if env_conditions == 'wet':
+        if mean <= 10.4:
+            limit = 21.2
+        elif mean <= 30:
+            limit = 16.5 + 0.45 * mean
+        else:
+            # This should likely be a failure, but technically is undefined
+            raise ValueError('DC values greater than 30 V are not supported for'
+                    f' let-go under wet conditions: {mean} V')
+    elif env_conditions == 'dry':
+        if mean <= 20.9:
+            limit = 42.4
+        elif mean <= 60:
+            limit = 33 + 0.45 * mean
+        else:
+            # This should likely be a failure, but technically is undefined
+            raise ValueError('DC values greater than 60 V are not supported for'
+                    f' let-go under dry conditions: {mean} V')
+
+    return peak <= limit
--- ul1400_1_analyzer/utils/waveform.py
+++ ul1400_1_analyzer/utils/waveform.py
@@ -5,3 +5,160 @@ fashion.
 Module Attributes:
   N/A
 """
+import copy
+import itertools
+from typing import Iterable
+
+
+
+def merge_regions(regions:list[tuple[float, float]]) \
+        -> list[tuple[float, float]]:
+    """
+    Merges all overlapped and exactly-adjacent regions in the list provided
+    until there are no overlapping nor exactly-adjacent regions remaining.  An
+    exactly-adjacent region is one where the start of one region and the end of
+    the other is the same exact value.
+
+    Args:
+      regions: The regions to merge, where each entry is a start and end value
+        pair.
+
+    Returns:
+      merged_regions: The regions represented by the ones provided, but with any
+        overlapped or exactly-adjacent regions combined recursively into a
+        single entry, where each entry is a start and end value pair.
+    """
+    merged_regions = copy.deepcopy(regions)
+    regions_to_remove:Iterable[tuple[float, float]] = []
+    need_another_iteration = True
+
+    while need_another_iteration:
+        need_another_iteration = False
+        for region_pair in itertools.combinations(merged_regions, 2):
+            union_region = unionize_regions(*region_pair)
+            if union_region is not None:
+                regions_to_remove = region_pair
+                need_another_iteration = True
+                break
+        if need_another_iteration:
+            for region in regions_to_remove:
+                merged_regions.remove(region)
+            merged_regions.append(union_region)
+
+    return merged_regions
+
+
+
+def expand_region(existing:tuple[float, float]|None,
+        addition:tuple[float, float]) -> tuple[float, float]:
+    """
+    Expands the existing region to also include the additional region provided.
+    If there is no existing region, the result will simply be the additional
+    region.
+
+    Args:
+      existing: The existing region to expand.  Can be None if there is no
+        existing region.
+      addition: The additional region to add to the existing region.  Must be
+        overlapped or exactly-adjacent.
+
+    Returns:
+      _: The union of the regions defined by the existing and addition regions,
+        or simply the addition region if the existing region did not exist.
+
+    Raises:
+      ValueError: Raised when the existing and addition regions are provided and
+        exist, but are not overlapped nor exactly-adjacent.
+    """
+    if existing is None:
+        return addition
+
+    union_region = unionize_regions(existing, addition)
+    if union_region is None:
+        raise ValueError('Cannot expand region if addition is not overlapped'
+                ' nor exactly-adjacent')
+
+    return union_region
+
+
+
+def unionize_regions(region_1:tuple[float, float],
+        region_2:tuple[float, float]) -> tuple[float, float]|None:
+    """
+    Finds the union of the 2 provided regions and returns the result if
+    possible.
+
+    Args:
+      region_1: One of the regions to combine with union logic, where the values
+        are a start and end value pair.
+      region_2: One of the regions to combine with union logic, where the values
+        are a start and end value pair.
+
+    Returns:
+      _: The new region that extends from the lowest start value of the 2
+        regions to the highest end value of the 2 regions, provided that the
+        combination of regions is continuous.  None if the 2 provided regions
+        are neither overlapped nor exactly-adjacent.
+    """
+    if region_1[1] < region_2[0] or region_2[1] < region_1[0]:
+        return None
+
+    return (min(region_1[0], region_2[0]), max(region_1[1], region_2[1]))
+
+
+
+def get_segment_from_waveform(waveform:dict[float, float]|None,
+        start_time:float, end_time:float, min_duration:float,
+        direction_to_extend_beyond:str) \
+        -> dict[float, float]|None:
+    """
+    Gets a segment from a waveform based on the start and end times provided.  A
+    minimum time window duration is enforced, where additional points are
+    included from the direction indicated until the minimum duration is met.
+
+    Scaling of units can be anything so long as they are consistent for all
+    arguments and are expected in the return results.
+
+    Args:
+      waveform: The waveform data from which to extract a segment based on a
+        specified time window.  Each entry is a point on the waveform, with the
+        key being the time and the value being the value at that time.  Time
+        value keys in the dictionary do not need to be ordered.
+      start_time: The start time of the segment to extract, inclusive.
+      end_time:  The end time of the segment to extract, inclusive
+      min_duration: The minimum duration that must exist in the segment after
+        extraction.  If this is not met, additional data will be included by
+        exceeding either the start time specified or the end time specified
+        based on the extension direction specified.
+      direction_to_extend_beyond: The direction in which to progress to include
+        additional data if the minimum duration is not met.  For example, if the
+        minimum duration is not met and this is specified as "start", then this
+        will include data below the start time provided.  Valid values are
+        "start" and "end".
+
+    Returns:
+      _: The segment of the waveform that includes the data at least from the
+        start time to the end time, with additional data included in the
+        direction specified until the minimum duration is met.  None if no
+        waveform provided or there is insufficient data in the waveform to meet
+        the minimum duration based on the specified parameters.
+    """
+    if waveform is None:
+        return None
+
+    segment = {t: v for t, v in waveform.items() if start_time <= t <= end_time}
+
+    try:
+        while max(segment.keys()) - min(segment.keys()) < min_duration:
+            if direction_to_extend_beyond == 'start':
+                next_earlier_time = max({t: v for t, v in waveform.items()
+                        if t < min(segment.keys())}.keys())
+                segment[next_earlier_time] = waveform[next_earlier_time]
+            elif direction_to_extend_beyond == 'end':
+                next_later_time = min({t: v for t, v in waveform.items()
+                        if t > max(segment.keys())}.keys())
+                segment[next_later_time] = waveform[next_later_time]
+    except ValueError:
+        return None
+
+    return segment
