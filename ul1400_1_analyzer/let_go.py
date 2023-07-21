f32d746d006198363e1d0beab9c80a2fe74c6a0f
Jonathan Casey <jonathan.casey@voltserver.com>
2023-07-21 15:36:16 -0400

    Restructured let-go analysis to support multi-core processing.

--- ul1400_1_analyzer/let_go.py
+++ ul1400_1_analyzer/let_go.py
@@ -6,6 +6,9 @@ Module Attributes:
   N/A
 """
 import math
+import multiprocessing as mp
+import os
+from typing import Any
 
 from alive_progress import alive_bar                      # type: ignore[import]
 
@@ -13,10 +16,32 @@ from ul1400_1_analyzer.utils import waveform as waveform_utils
 
 
 
+_PROGRESS_COUNTER = None
+
+
+
+def _init_shared_data(shared_data:Any) -> None:
+    """
+    Initializes any data that is to be shared across processes when using
+    multiprocessing.
+
+    This is intended to be called at the start of each process.
+
+    Args:
+      shared_data: The data that is to be shared.  At this time, this is a tuple
+        that only includes the reference to the global progress counter (which
+        must be created prior to this call).
+    """
+    global _PROGRESS_COUNTER                  # pylint: disable=global-statement
+    _PROGRESS_COUNTER = shared_data
+
+
+
 def find_time_regions_below_letgo(current_waveform:dict[float, float]|None=None,
         voltage_waveform:dict[float, float]|None=None,
         env_conditions:str|None=None, start_time:float|None=None,
-        min_window_duration:float=3) -> tuple[tuple[float, float], ...]:
+        min_window_duration:float=3, num_cores:int|None=None) \
+        -> tuple[tuple[float, float], ...]:
     """
     Finds the time regions that are below the let-go threshold.  When the
     appropriate minimum recovery duration is provided, this can be used to
@@ -49,6 +74,7 @@ def find_time_regions_below_letgo(current_waveform:dict[float, float]|None=None,
       min_window_duration [s]: The minimum time duration to use as a window
         size for evaluating data.  For compliance, this should likely be the
         minimum Fault Recovery Period duration required by UL1400-1.
+      num_cores: The number of cores to use.  Can be omitted to use all cores.
 
     Returns:
       time_regions_below_letgo [s]: These are the time regions identified as
@@ -67,50 +93,121 @@ def find_time_regions_below_letgo(current_waveform:dict[float, float]|None=None,
     }
     time_regions_below_letgo:list[tuple[float, float]] = []
 
+    if num_cores is None:
+        num_cores = os.cpu_count()
+    num_processes = min(num_cores, os.cpu_count())
+
+    last_error_message = None
+    def _error_handler(ex:Exception) -> None:
+        """
+        Internal error handler for processes in the multiprocessing pool.
+
+        Args:
+          ex: The exception encountered in the process.
+        """
+        nonlocal last_error_message
+        last_error_message = str(ex)
+
+
     for measurement_type, waveform in both_waveforms.items():
         if waveform is None:
             continue
 
-        working_region = None
         min_start_time = start_time if start_time is not None else min(
                 waveform.keys())
-        target_end_time = max(waveform.keys())
-        target_start_time:float|None = target_end_time - min_window_duration
+        target_times = [(t - min_window_duration, t) for t in waveform.keys()
+                if t - min_window_duration >= min_start_time]
 
-        approx_len = len([t for t in waveform.keys()
-                if t >= min_start_time + min_window_duration])
+        global _PROGRESS_COUNTER              # pylint: disable=global-statement
+        _PROGRESS_COUNTER = mp.Value('i', 0)
 
-        with alive_bar(approx_len,
-                title=f'Analyzing {measurement_type} waveform') as progress_bar:
-            while target_start_time is not None \
-                    and target_start_time >= min_start_time:
-                segment = waveform_utils.get_segment_from_waveform(waveform,
-                        target_start_time, target_end_time, min_window_duration,
-                        'start')
+        with mp.Pool(num_processes, initializer=_init_shared_data,
+                initargs=(_PROGRESS_COUNTER, )) as pool:
+            with alive_bar(len(target_times),
+                    title=f'Analyzing {measurement_type} waveform') \
+                    as progress_bar:
 
-                if is_below_letgo(measurement_type, segment.values(),
-                        env_conditions):
-                    new_region = (min(segment.keys()), max(segment.keys()))
-                    working_region = waveform_utils.expand_region(
-                            working_region, new_region)
-                elif working_region is not None:
-                    time_regions_below_letgo.append(working_region)
-                    working_region = None
+                args = [(measurement_type, waveform, *t, min_window_duration,
+                            env_conditions) for t in target_times]
+                future_result = pool.starmap_async(_analyze_segment, args,
+                        error_callback=_error_handler)
 
-                remaining_end_times = [t for t in waveform.keys()
-                        if t < target_end_time]
-                if remaining_end_times:
-                    target_end_time = max(remaining_end_times)
-                    target_start_time = target_end_time - min_window_duration
+                while not future_result.ready():
+                    if _PROGRESS_COUNTER.value != 0:
+                        with _PROGRESS_COUNTER.get_lock():
+                            increment = _PROGRESS_COUNTER.value
+                            _PROGRESS_COUNTER.value = 0
+                        progress_bar(increment)   # pylint: disable=not-callable
+
+                if not future_result.successful():
+                    print(last_error_message)
                 else:
-                    target_start_time = None
+                    time_regions_below_letgo.extend(
+                            [r for r in future_result.get() if r is not None])
 
-                progress_bar()                    # pylint: disable=not-callable
+                pool.close()
+                pool.join()
 
-        if working_region is not None:
-            time_regions_below_letgo.append(working_region)
+        _PROGRESS_COUNTER = None
 
+    print('Merging results...')
     return waveform_utils.merge_regions(time_regions_below_letgo)
+
+
+
+def _analyze_segment(measurement_type:str, waveform:dict[float, float]|None,
+        target_start_time:float, target_end_time:float,
+        min_window_duration:float, env_conditions:str|None) \
+        -> tuple[float, float]|None:
+    """
+    Finds the segment of data to analyze and performs the let-go analysis.
+
+    This is intended to be run on a separate process/thread for parallel
+    computing, but it is not required.
+
+    Args:
+      measurement_type: The type of measurement represented in the segment data
+        provided.  Valid values are "current" and "voltage".
+      waveform: The current or voltage waveform data from which to extract a
+        segment based on a specified time window.  Each entry is a point on the
+        waveform, with the key being the time and the value being the value at
+        that time.  Time value keys in the dictionary do not need to be ordered.
+      target_start_time: The start time of the segment to extract, inclusive.
+        This is an ideal target start time; depending on the data in the
+        waveform and the other constraints, this may not be the exact start time
+        used.  This value may be exceeded by a lower value if additional data is
+        needed to meet the minimum window duration.
+      target_end_time: The end time of the segment to extract, inclusive.  This
+        is an ideal target end time; depending on the data in the waveform and
+        the other constraints, this may not be the exact end time used.  This
+        value may not be present in the data at all, and it certainly will not
+        be exceeded.
+      min_window_duration [s]: The minimum time duration to use as a window
+        size for evaluating data.  For compliance, this should likely be the
+        minimum Fault Recovery Period duration required by UL1400-1.
+      env_conditions: The environmental conditions to be used for evaluating
+        let-go.  This is only required if the fault voltage waveform is
+        provided, as this is the only waveform to which it applies.  The valid
+        values are "wet" and "dry".
+
+    Returns:
+      new_passing_region: The new region, defined as a start and end time, that
+        passes the let-go threshold.  If test failed, this will be None.
+    """
+    new_passing_region = None
+    segment = waveform_utils.get_segment_from_waveform(waveform,
+            target_start_time, target_end_time, min_window_duration,
+            'start')
+
+    if is_below_letgo(measurement_type, segment.values(),
+            env_conditions):
+        new_passing_region = (min(segment.keys()), max(segment.keys()))
+
+    if _PROGRESS_COUNTER is not None:
+        with _PROGRESS_COUNTER.get_lock():
+            _PROGRESS_COUNTER.value += 1
+
+    return new_passing_region
 
 
 
